9d7206d0528cb6be91c24876b4fc528ac3f3cc6c
farouqdaif1 <daiffarouq@gmail.com>
2021-10-04 21:54:58 +0200

    fix disctop version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-const menu = document.querySelector('.mob-nav');
+const menu = document.querySelector('#burger');
+const close = document.querySelector('#close');
+const option = document.querySelector('.toul');
 function showMe() {
   document.querySelector('.nav-ul').classList.toggle('open');
   document.querySelector('#close').classList.toggle('open');
   document.querySelector('.bur-btn').classList.toggle('hide');
+  document.querySelector('.close').classList.toggle('hide');
+  document.querySelector('.toul').classList.toggle('hide');
 }
 menu.addEventListener('click', showMe);
+close.addEventListener('click', showMe);
+option.addEventListener('click', showMe);
