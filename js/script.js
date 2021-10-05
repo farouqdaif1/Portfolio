5d32bc983a1852446b185fa10cd5f5910929ad28
farouqdaif1 <daiffarouq@gmail.com>
2021-10-05 14:18:09 +0200

    fix disctop version and mobile version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const menu = document.querySelector('#burger');
 const close = document.querySelector('#close');
 const ul2 = document.querySelector('.ul2');
-
 function showMe() {
   document.querySelector('.nav-ul').classList.toggle('open');
   document.querySelector('#close').classList.toggle('open');
+  document.querySelector('#ml').classList.toggle('open');
 }
 menu.addEventListener('click', showMe);
 close.addEventListener('click', showMe);
